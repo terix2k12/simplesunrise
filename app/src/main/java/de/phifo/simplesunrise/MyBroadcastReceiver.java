25acce5bd649a42edbb18ad596a4dc6d5c225cd5
terix2k12 <terixmail@googlemail.com>
2018-09-22 19:49:13 +0200

    Formatting and cleanup

--- app/src/main/java/de/phifo/simplesunrise/MyBroadcastReceiver.java
+++ app/src/main/java/de/phifo/simplesunrise/MyBroadcastReceiver.java
@@ -3,26 +3,16 @@ package de.phifo.simplesunrise;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
-public class MyBroadcastReceiver  extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            long mili = System.currentTimeMillis();
+public class MyBroadcastReceiver extends BroadcastReceiver {
 
-//            SunriseActivity activity = (SunriseActivity) context;
-
-//            activity.setText("I was called!");
-
-            Toast.makeText(context, "Don't panik but your time "+mili+" is up!!!!.",
-                    Toast.LENGTH_LONG).show();
-
-            Intent mainIntent = new Intent(context, SunriseActivity.class);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            mainIntent.putExtra(SunriseActivity.EXTRA_STARTMODE, "ring");
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent mainIntent = new Intent(context, SunriseActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mainIntent.putExtra(SunriseActivity.EXTRA_STARTMODE, SunriseActivity.STARTMODE_ALERT);
 
         context.startActivity(mainIntent);
-        }
-
     }
+
+}
--- app/src/main/java/de/phifo/simplesunrise/SunriseActivity.java
+++ app/src/main/java/de/phifo/simplesunrise/SunriseActivity.java
@@ -4,9 +4,8 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-//
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -19,29 +18,27 @@ import java.util.Calendar;
 
 public class SunriseActivity extends AppCompatActivity {
 
-    static final String EXTRA_STARTMODE = "de.phifo.simplesunrise.EXTRA_STARTMODE";
+    public static final String EXTRA_STARTMODE = "de.phifo.simplesunrise.EXTRA_STARTMODE";
+    public static final String STARTMODE_ALERT = "ring";
 
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
 
     private TextView textView;
 
-    public void setText(String text)
-    {
+    public void setText(String text) {
         textView.setText(text);
     }
 
-    public void lockScreen(boolean lock)
-    {
-        if(lock)
-        {
+    public void lockScreen(boolean lock) {
+        if (lock) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }else{
+        } else {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
     }
 
-    private void startAler(){
+    private void startAlert() {
         int i = 15;
 
 //        alarmMgr.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
@@ -50,8 +47,8 @@ public class SunriseActivity extends AppCompatActivity {
         // Set the alarm to start at 8:30 a.m.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 23);
-        calendar.set(Calendar.MINUTE, 27);
+        calendar.set(Calendar.HOUR_OF_DAY, 19);
+        calendar.set(Calendar.MINUTE, 45);
 //        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
 //                          AlarmManager.INTERVAL_DAY, alarmIntent);
 
@@ -59,12 +56,14 @@ public class SunriseActivity extends AppCompatActivity {
 
         SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
+        long diff = millis - System.currentTimeMillis();
+
+        setText(" Alarm in Minutes from now: " + (diff / 1000 / 60)+"  ");
 
         alarmMgr.set(AlarmManager.RTC_WAKEUP, millis, alarmIntent);
 
-        Toast.makeText(this, "Alarm set in " + i + " seconds" +
-                        format1.format(calendar.getTime()),
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Alarm set in " + i + "  " +
+                format1.format(calendar.getTime()), Toast.LENGTH_LONG).show();
 
     }
 
@@ -76,35 +75,43 @@ public class SunriseActivity extends AppCompatActivity {
 
         textView = new TextView(this);
 
-        if(getIntent().getExtras()!=null)
-        {
+        if (getIntent().getExtras() != null) {
             Object startmode = getIntent().getExtras().get(EXTRA_STARTMODE);
-            if(startmode == null)
-            {
+            if (startmode == null) {
                 setText("Sleeping...");
-            }    else{
+            } else {
                 setText("Ringing...");
                 lockScreen(true);
             }
-        }
-        else{
+        } else {
             setText("First?!...");
         }
 
         Button buttonView = new Button(this);
+        buttonView.setText("Set Alert");
         buttonView.setOnClickListener(
                 new View.OnClickListener() {
-                                          @Override
-                                          public void onClick(View v) {
-                            textView.setText("Hello World.");
+                    @Override
+                    public void onClick(View v) {
+                        textView.setText("Hello World.");
 
-                                startAler();
+                        startAlert();
 
-                            // lockScreen(false);
-                                          }
-                                      }
+                        // lockScreen(false);
+                    }
+                }
         );
 
+        Button buttonLight = new Button(this);
+        buttonLight.setText("Turn on Light");
+        buttonView.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        makeBright();
+                    }
+                }
+        );
 
 
         linearLayout.addView(textView);
@@ -113,7 +120,7 @@ public class SunriseActivity extends AppCompatActivity {
         setContentView(linearLayout);
 
 
-        alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
         Intent intent = new Intent(this, MyBroadcastReceiver.class);
 
@@ -121,12 +128,17 @@ public class SunriseActivity extends AppCompatActivity {
                 0, intent, 0);
 
 
-
 // setRepeating() lets you specify a precise custom interval--in this case,
 // 20 minutes.
- //       alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
- //               1000 * 60 * 20, alarmIntent);
+        //       alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+        //               1000 * 60 * 20, alarmIntent);
 
-      //
+        //
+    }
+
+    private void makeBright(){
+
+        
+
     }
 }
