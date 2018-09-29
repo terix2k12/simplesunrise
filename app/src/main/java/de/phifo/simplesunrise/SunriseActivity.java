02f8a413102d2a59d1f465eaa9da3969cebd44f5
terix2k12 <terixmail@googlemail.com>
2018-09-29 16:20:57 +0200

    Add test button

--- app/src/main/java/de/phifo/simplesunrise/SunriseActivity.java
+++ app/src/main/java/de/phifo/simplesunrise/SunriseActivity.java
@@ -43,9 +43,6 @@ public class SunriseActivity extends AppCompatActivity implements ITimeConsumer 
 //                          AlarmManager.INTERVAL_DAY, alarmIntent);
 
 
-//        long diff = millis - System.currentTimeMillis();
-//        setText(" Alarm in Minutes from now: " + (diff / 1000 / 60) + "  ");
-
         alarmMgr.set(AlarmManager.RTC_WAKEUP, millis, alarmIntent);
 
         // from api lvl 21
@@ -56,8 +53,14 @@ public class SunriseActivity extends AppCompatActivity implements ITimeConsumer 
         SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(millis);
-        Toast.makeText(this, "Alarm set to: " +
-                format1.format(calendar.getTime()), Toast.LENGTH_LONG).show();
+
+        long diff = millis - System.currentTimeMillis();
+        String fromTxt = " Alarm in Minutes from now: " + (diff / 1000 / 60) + "  ";
+        Toast.makeText(this, fromTxt, Toast.LENGTH_LONG).show();
+
+        String textSetTo = "Alarm set to: " +
+                format1.format(calendar.getTime());
+        textView.setText(textSetTo);
     }
 
     @Override
@@ -80,27 +83,41 @@ public class SunriseActivity extends AppCompatActivity implements ITimeConsumer 
                 }
         );
 
+        Button buttonTest = new Button(this);
+        buttonTest.setText("Test Alert");
+        buttonTest.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent testIntent = new Intent(SunriseActivity.this, SunriseWakeupActivity.class);
+                        testIntent.putExtra(SunriseActivity.EXTRA_STARTMODE, SunriseActivity.STARTMODE_ALERT);
+
+                        startActivity(testIntent);
+                    }
+                }
+        );
+
         linearLayout.addView(textView);
         linearLayout.addView(buttonView);
+        linearLayout.addView(buttonTest);
 
         setContentView(linearLayout);
     }
 
-    public void onTimeSet(int hourOfDay, int minutes){
+    public void onTimeSet(int hourOfDay, int minutes) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
 
-         if(calendar.get(Calendar.HOUR_OF_DAY) > hourOfDay)
-         {
+        if (calendar.get(Calendar.HOUR_OF_DAY) > hourOfDay) {
             calendar.add(Calendar.DAY_OF_YEAR, 1);
-         }else if(calendar.get(Calendar.HOUR_OF_DAY) == hourOfDay &&
-                  calendar.get(Calendar.MINUTE) >= minutes
-                 ){
-             calendar.add(Calendar.DAY_OF_YEAR, 1);
-         }
+        } else if (calendar.get(Calendar.HOUR_OF_DAY) == hourOfDay &&
+                calendar.get(Calendar.MINUTE) >= minutes
+                ) {
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+        }
 
         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        calendar.set(Calendar.MINUTE, minutes-5);
+        calendar.set(Calendar.MINUTE, minutes - 5);
         calendar.set(Calendar.SECOND, 0);
 
         startAlert(calendar.getTimeInMillis());
